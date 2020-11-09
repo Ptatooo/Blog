add4af21cdd43f65f6d09c3bfacb2ea9e8f87f56
Ptatooo <kotoricdino52069@gmail.com>
2020-11-09 20:54:54 +0100

    1 commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  # For d{etails on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
