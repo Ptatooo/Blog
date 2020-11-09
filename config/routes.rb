afa99c7fea76b53ea74c9a253343a286c3970e43
Ptatooo <kotoricdino52069@gmail.com>
2020-11-09 21:29:37 +0100

    Authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   # For d{etails on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'posts#index'
 end
